097528fc7c020edbc5f2e050e0a68036cdac0d03
PaniShews <kpetruh@gmail.com>
2026-06-02 16:47:36 +0300

    Solution

--- cinema_service/urls.py
+++ cinema_service/urls.py
@@ -1,8 +1,7 @@
-# project/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/cinema/", include("cinema.urls")),  # add this
+    path("api/cinema/", include("cinema.urls")),
 ]
